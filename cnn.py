0c8addf325e06f41dd798c066262d5c4462a9a27
qazzgff <=qazzgff@hotmail.com>
2021-03-11 16:10:34 +0000

    update

--- cnn.py
+++ cnn.py
@@ -44,21 +44,21 @@ model.add(Dense(10,activation='softmax',kernel_initializer='he_normal'))
 
 
 model.summary()
-model.compile(optimizer='adam',
+model.compile(optimizer='SGD',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-history = model.fit(train_images, train_labels, epochs=10, 
+history = model.fit(train_images, train_labels, epochs=30, 
                     validation_data=(test_images, test_labels))
 
 plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
+plt.ylim([0, 1])
 plt.legend(loc='lower right')
 plt.show()
 
-test_loss, test_acc = vgg16.evaluate(test_images,  test_labels, verbose=2)
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 print(test_acc)
 
 
